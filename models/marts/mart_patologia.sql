693b8a66de3f87d3221850ebb4b5e9759eeae99c
Tim Ryan <ryantimjohn@gmail.com>
2024-11-18 17:37:15 +0000

    sqlfmt

--- models/marts/mart_patologia.sql
+++ models/marts/mart_patologia.sql
@@ -1,13 +1,18 @@
-with unioned as ({{
-    dbt_utils.union_relations(
-        relations=[
-            ref("stg_laboratorio"),
-            ref("stg_medicina_general"),
-            ref("stg_odontologia"),
-            ref("stg_psicologia"),
-        ]
+with
+    unioned as (
+        {{
+            dbt_utils.union_relations(
+                relations=[
+                    ref("stg_laboratorio"),
+                    ref("stg_medicina_general"),
+                    ref("stg_odontologia"),
+                    ref("stg_psicologia"),
+                ]
+            )
+        }}
     )
-}})
-SELECT *,
-{{clean_illness_value('enfermedad_o_tratamiento')}} as enfermedad_o_tratamiento_limpiado
+select
+    *,
+    {{ clean_illness_value("enfermedad_o_tratamiento") }}
+    as enfermedad_o_tratamiento_limpiado
 from unioned
