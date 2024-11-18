250b3850d6e0af159c48e05798fd1e915481b34d
Tim Ryan <ryantimjohn@gmail.com>
2024-11-18 20:09:54 +0000

    add array aggregation and extraction metric

---
+++ models/reports/report_visita_a_odontologia_incluyo_extraccion.sql
@@ -0,0 +1,19 @@
+select
+    ts,
+    fecha,
+    medico,
+    edad_de_paciente,
+    edad_sort,
+    mes,
+    ano,
+    visit_number,
+    servicio,
+    case
+        when
+            'Extracción Dientes Permanentes'
+            in unnest(enfermedad_o_tratamiento_limpiado_array)
+        then 1
+        else 0
+    end visita_incluyo_extraccion
+from {{ ref("mart_patologia_agregado_por_visita") }}
+where servicio = 'odontlogia'
